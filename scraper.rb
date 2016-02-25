5bca2276ff7dcae9921dbd072018ff0c28880f55
Luke Bacon <luke@equivalentideas.com>
2016-02-25 11:35:32 +1100

    Add some whitespace to make reading code easier (whitespace only)

--- scraper.rb
+++ scraper.rb
@@ -11,21 +11,26 @@ councils.each do |council|
   #p council
   council_name = council["council_name"]
   council_website = council["website_url"]
+
   record = {
     "councillor" => council["mayor"],
     "position" => "mayor",
     "council_name" => council_name,
     "council_website" => council_website
   }
+
   p record
   ScraperWiki.save_sqlite(["councillor", "council_name"], record)
+
   councillors = council["councilors"]
+
   councillors.each do |councillor|
     record = {
       "councillor" => councillor["name"],
       "council_name" => council_name,
       "council_website" => council_website
     }
+
     p record
     ScraperWiki.save_sqlite(["councillor", "council_name"], record)
   end
